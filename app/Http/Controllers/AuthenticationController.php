37b74fc61aa25262adff76aa04643feffa4ac5be
Afiss <afisfisabilillah21@gmail.com>
2026-05-17 04:30:47 +0700

    feat: add login with token

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 class AuthenticationController extends Controller
 {
-    public function index()
+    public function login(LoginRequest $request)
     {
+        $loginRequest = $request->validated();
+        $user = User::where('email', $loginRequest['email'])->first();
+        if(!$user || !Hash::check($loginRequest['password'], $user->password) ){
+//            Login gagal
+            throw ValidationException::withMessages([
+                'email' => ['Email atau password salah.'],
+            ]);
+        }
 
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            "message"=>"login berhasil",
+            "token" => $token,
+            "user" => [
+                "id" => $user->id,
+                "name" => $user->name,
+                "email" => $user->email,
+                "fullname" => $user->fullname,
+                "profile" => $user->profile,
+            ]
+        ]);
     }
 }
